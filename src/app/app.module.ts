c23dc69f2fe2eb0d8fbf50226f7e01d86394c198
Alexander Hridin <alexander.m.gridin@gmail.com>
2022-07-24 15:18:25 +0300

    moved modules folder from shared to core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from '@core/routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppLayoutModule } from '@shared/modules/app-layout/app-layout.module';
-import { HeaderModule } from '@shared/modules/header/header.module';
-import { SidebarContainerModule } from '@shared/modules/sidebar-container/sidebar-container.module';
-import { TabsModule } from '@shared/modules/tabs/tabs.module';
+import { AppLayoutModule } from '@core/modules/app-layout/app-layout.module';
+import { HeaderModule } from '@core/modules/header/header.module';
+import { SidebarContainerModule } from '@core/modules/sidebar-container/sidebar-container.module';
+import { TabsModule } from '@core/modules/tabs/tabs.module';
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/core/modules/tabs/services/tabs/tabs.service.ts
+++ src/app/core/modules/tabs/services/tabs/tabs.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { Tab } from '@shared/modules/tabs/models';
+import { Tab } from '@core/modules/tabs/models';
 
 @Injectable()
 export class TabsService {
