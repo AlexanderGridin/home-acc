fb164034173844cc85d4510cd1f6ed24f21fc4a8
Alexander Hridin <alexander.m.gridin@gmail.com>
2022-07-31 23:58:08 +0300

    cleanup

--- src/app/core/services/pages/pages.service.ts
+++ src/app/core/services/pages/pages.service.ts
@@ -2,21 +2,21 @@ import { ComponentRef, Injectable } from '@angular/core';
 
 @Injectable()
 export class PagesService {
-  private readonly _componentsRefsMap: Record<string, ComponentRef<any>> = {};
+  private readonly componentsRefsMap: Record<string, ComponentRef<any>> = {};
 
   public registerComponentRef(
     componentRef: ComponentRef<any>,
     url: string
   ): void {
-    if (this._componentsRefsMap[url]) {
+    if (this.componentsRefsMap[url]) {
       return;
     }
 
-    this._componentsRefsMap[url] = componentRef;
+    this.componentsRefsMap[url] = componentRef;
   }
 
   public hideAllPages(): void {
-    const urls: Array<string> = Object.keys(this._componentsRefsMap);
+    const urls: Array<string> = Object.keys(this.componentsRefsMap);
 
     if (!urls.length) {
       return;
@@ -24,26 +24,26 @@ export class PagesService {
 
     urls.forEach(
       (url: string) =>
-        (this._componentsRefsMap[url].location.nativeElement.hidden = true)
+        (this.componentsRefsMap[url].location.nativeElement.hidden = true)
     );
   }
 
   public showPage(url: string): void {
-    const urls: Array<string> = Object.keys(this._componentsRefsMap);
+    const urls: Array<string> = Object.keys(this.componentsRefsMap);
     const activeUrl: string | undefined = urls.find(
       (url: string) =>
-        !this._componentsRefsMap[url].location.nativeElement.hidden
+        !this.componentsRefsMap[url].location.nativeElement.hidden
     );
 
     if (activeUrl) {
-      this._componentsRefsMap[activeUrl].location.nativeElement.hidden = true;
+      this.componentsRefsMap[activeUrl].location.nativeElement.hidden = true;
     }
 
-    this._componentsRefsMap[url].location.nativeElement.hidden = false;
+    this.componentsRefsMap[url].location.nativeElement.hidden = false;
   }
 
   public removePage(url: string): number {
-    const urls: Array<string> = Object.keys(this._componentsRefsMap);
+    const urls: Array<string> = Object.keys(this.componentsRefsMap);
     const componentRefIndex: number = urls.findIndex(
       (existingUrl) => existingUrl === url
     );
@@ -52,13 +52,13 @@ export class PagesService {
       return -1;
     }
 
-    this._componentsRefsMap[url].destroy();
-    delete this._componentsRefsMap[url];
+    this.componentsRefsMap[url].destroy();
+    delete this.componentsRefsMap[url];
 
     return componentRefIndex;
   }
 
   public getComponentRef(url: string): ComponentRef<any> | undefined {
-    return this._componentsRefsMap[url];
+    return this.componentsRefsMap[url];
   }
 }
