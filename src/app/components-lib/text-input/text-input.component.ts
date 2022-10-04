22549ff05012dbd7967591439f17473743c48c6c
Alexander Hridin <alexander.m.gridin@gmail.com>
2022-10-04 22:34:31 +0300

    cleanup of the text-input.component

--- src/app/components-lib/text-input/text-input.component.ts
+++ src/app/components-lib/text-input/text-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControlBase } from '@components-lib/shared/form-control-base.component';
 
 @Component({
@@ -6,6 +6,4 @@ import { FormControlBase } from '@components-lib/shared/form-control-base.compon
   templateUrl: './text-input.component.html',
   styleUrls: ['./text-input.component.scss'],
 })
-export class TextInputComponent extends FormControlBase implements OnInit {
-  ngOnInit(): void {}
-}
+export class TextInputComponent extends FormControlBase {}
