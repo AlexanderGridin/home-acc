5f07acde4dc634f8c644dced2a63d9fd6fa8d318
Alexander Hridin <alexander.m.gridin@gmail.com>
2022-10-04 23:59:03 +0300

    app.module updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { CoreModule } from '@core/core.module';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, CoreModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CoreModule,
+    MatSelectModule,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
