c463182606aa00aa06c10f83d67d03bb17da90fa
Alexander Hridin <alexander.m.gridin@gmail.com>
2022-10-04 22:40:59 +0300

    form-control-base.component updated

--- src/app/components-lib/shared/form-control-base.component.ts
+++ src/app/components-lib/shared/form-control-base.component.ts
@@ -1,12 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Subject } from 'rxjs';
 
 @Component({
   template: '',
 })
-export abstract class FormControlBase {
+export abstract class FormControlBase implements OnDestroy {
   @Input() public control: FormControl = new FormControl(null);
   @Input() public label = '';
   @Input() public placeholder = '';
   @Input() public id = '';
+
+  protected readonly destroy$: Subject<void> = new Subject<void>();
+
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
