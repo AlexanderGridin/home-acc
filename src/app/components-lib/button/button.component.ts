84f5910611b8e8788e454cd4c8e802e4a4cdc0d0
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2022-10-23 23:22:36 +0300

    added disabling

--- src/app/components-lib/button/button.component.ts
+++ src/app/components-lib/button/button.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class ButtonComponent {
   @Input() public text = 'Button';
   @Input() public icon = '';
+  @Input() public disabled = false;
 
   @Output() public onClick: EventEmitter<void> = new EventEmitter<void>();
 
